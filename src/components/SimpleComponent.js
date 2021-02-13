5eb2de0e0c95bc700b85bbaa5bd1640fe40891c8
patrick-rush <patrick.a.rush@gmail.com>
2021-02-13 18:57:02 -0500

    commit tree 0c6ed136f14e65cf18b53c6111040773600d11c9

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,24 @@
 // Code SimpleComponent Here
+import React, { Component } from 'react';
+
+export default class SimpleComponent extends Component {
+    state = {
+        mood: "happy"
+    }
+
+    handleClick = () => {
+        this.setState(function(previousState) {
+            if (previousState.mood === "happy") {
+                return {mood: "sad"}
+            } else {
+                return {mood: "happy"}
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div onClick={this.handleClick}>{this.state.mood}</div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={function() {alert("It's true!")}} />
   </div>,
   document.getElementById('root')
 );
